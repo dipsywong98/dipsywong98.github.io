4991638dae45743a68319a4062de6af0976d499d
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2024-06-14 22:08:39 +0800

    fix icon in dark theme

--- src/components/v2/SvgLink.tsx
+++ src/components/v2/SvgLink.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React, { type ReactNode } from 'react'
 import { ToolTip } from './ToolTip'
 import NextImage from 'next/image'
+import { useTheme } from 'next-themes'
 
 interface Props {
   title: string
@@ -10,10 +13,12 @@ interface Props {
 }
 
 export const SvgLink = ({ title, href, icon }: Props): ReactNode => {
+  let { resolvedTheme } = useTheme()
+  const iconUrl = resolvedTheme !== 'dark' ? `/svg/${icon}.svg` : `/svg/dark/${icon}.svg`
   return (
     <ToolTip title={title}>
       <a href={href}>
-        <NextImage width={32} height={32} src={`/svg/${icon}.svg`} alt="icon" />
+        <NextImage width={32} height={32} src={iconUrl} alt="icon" />
       </a>
     </ToolTip>
   )
