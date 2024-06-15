2c06784f6a2a6e8c2080e03b4e186ca05479b1b9
(bot) Dipsy Wong <ycwongal@connect.ust.hk>
2024-06-15 13:42:46 +0800

    make nginx note interactive

--- src/components/widgets/WidgetInput.tsx
+++ src/components/widgets/WidgetInput.tsx
@@ -4,10 +4,7 @@ import { useEffect } from "react"
 import { Input } from "../ui/input"
 import { useWidgetValues } from "./WidgetContext"
 
-export const WidgetInput = ({name, defaultValue = '', ...props}) => {
+export const WidgetInput = ({name, ...props}) => {
   const { getValue, setValue } = useWidgetValues()
-  useEffect(() => {
-    setValue(name, defaultValue)
-  }, [])
   return <Input value={getValue(name) ?? ''} onChange={({target}) => setValue(name, target.value)} placeholder={name} {...props} />
 }
